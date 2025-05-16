e76c40dfea426252e650a533f4b8eec3d7601711
gabriel.jaya@epn.edu.ec <eliath.velasco@gmail.com>
2025-05-16 10:29:12 -0500

    Added login activity

---
+++ app/src/main/java/com/velasco/eliath/cazarpatos/LoginActivity.kt
@@ -0,0 +1,20 @@
+package com.velasco.eliath.cazarpatos
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        val loginButton: Button = findViewById(R.id.button)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
